03c2bfa65af2020313bde19bb0dca84a6aa99d7c
Duncan Campbell <campbellduncan12@gmail.com>
2025-12-30 16:00:48 -0600

    HTTPApi patched
    from org.squiddev.cctweaks.lua.patch HTTPAPI_Patch

--- src/main/java/dan200/computercraft/core/apis/HTTPRequest.java
+++ src/main/java/dan200/computercraft/core/apis/HTTPRequest.java
@@ -1,146 +1,178 @@
 package dan200.computercraft.core.apis;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.StringReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import dan200.computercraft.ComputerCraft;
+import dan200.computercraft.api.lua.LuaException;
 
 public class HTTPRequest {
 
-    private Object m_lock = new Object();
-    private URL m_url;
-    private final String m_urlString;
-    private boolean m_complete;
-    private boolean m_cancelled;
-    private boolean m_success;
-    private String m_result;
-    private int m_responseCode;
-
-    public static URL checkURL(String urlString) throws HTTPRequestException {
+    public static URL checkURL(String urlString) throws LuaException {
         URL url;
         try {
             url = new URL(urlString);
-        } catch (MalformedURLException var9) {
-            throw new HTTPRequestException("URL malformed");
+        } catch (MalformedURLException e) {
+            throw new LuaException("URL malformed");
         }
 
         String protocol = url.getProtocol()
             .toLowerCase();
-        if (!protocol.equals("http") && !protocol.equals("https")) {
-            throw new HTTPRequestException("URL not http");
-        } else {
-            boolean allowed = false;
-            String whitelistString = ComputerCraft.http_whitelist;
-            String[] allowedURLs = whitelistString.split(";");
+        if (!protocol.equals("http") && !protocol.equals("https")) throw new LuaException("URL not http");
 
-            for (int i = 0; i < allowedURLs.length; i++) {
-                String allowedURL = allowedURLs[i];
-                Pattern allowedURLPattern = Pattern
-                    .compile("^\\Q" + allowedURL.replaceAll("\\*", "\\\\E.*\\\\Q") + "\\E$");
-                if (allowedURLPattern.matcher(url.getHost())
-                    .matches()) {
-                    allowed = true;
-                    break;
-                }
-            }
-
-            if (!allowed) {
-                throw new HTTPRequestException("Domain not permitted");
-            } else {
-                return url;
+        boolean allowed = false;
+        String whitelistString = ComputerCraft.http_whitelist;
+        String[] allowedURLs = whitelistString.split(";");
+        for (String allowedURL : allowedURLs) {
+            Pattern allowedURLPattern = Pattern.compile("^\\Q" + allowedURL.replaceAll("\\*", "\\\\E.*\\\\Q") + "\\E$");
+            if (allowedURLPattern.matcher(url.getHost())
+                .matches()) {
+                allowed = true;
+                break;
             }
         }
+
+        if (!allowed) throw new LuaException("Domain not permitted");
+
+        return url;
     }
 
-    public HTTPRequest(String url, final String postText, final Map<String, String> headers)
-        throws HTTPRequestException {
-        this.m_urlString = url;
-        this.m_url = checkURL(this.m_urlString);
-        this.m_cancelled = false;
-        this.m_complete = false;
-        this.m_success = false;
-        this.m_result = null;
-        this.m_responseCode = -1;
+    private final Object lock = new Object();
+    private URL url;
+    private final String urlString;
+    private boolean complete = false;
+    private boolean cancelled = false;
+    private boolean success = false;
+    private byte[] result;
+    private int responseCode = -1;
+    private Map<String, Map<Integer, String>> responseHeaders;
+
+    private static final String[] methods = { "GET", "POST", "HEAD", "OPTIONS", "PUT", "DELETE", "TRACE" };
+
+    private static boolean checkMethod(String verb) {
+        for (String method : methods) {
+            if (verb.equals(method)) return true;
+        }
+
+        return false;
+    }
+
+    public HTTPRequest(final String url, final String postText, final Map<String, String> headers, final String verb)
+        throws LuaException {
+        urlString = url;
+        this.url = checkURL(url);
+
+        if (verb != null && !checkMethod(verb)) throw new LuaException("No such verb: " + verb);
+
         Thread thread = new Thread(new Runnable() {
 
             @Override
             public void run() {
                 try {
-                    HttpURLConnection connection = (HttpURLConnection) HTTPRequest.this.m_url.openConnection();
-                    if (postText != null) {
-                        connection.setRequestMethod("POST");
-                        connection.setDoOutput(true);
-                    } else {
-                        connection.setRequestMethod("GET");
-                    }
+                    HttpURLConnection connection = (HttpURLConnection) HTTPRequest.this.url.openConnection();
 
-                    if (headers != null) {
-                        for (Entry<String, String> header : headers.entrySet()) {
-                            connection.setRequestProperty(header.getKey(), header.getValue());
+                    { // Setup connection
+                        if (verb != null) {
+                            connection.setRequestMethod(verb);
+                        } else if (postText != null) {
+                            connection.setRequestMethod("POST");
+                        } else {
+                            connection.setRequestMethod("GET");
                         }
-                    }
 
-                    if (postText != null) {
-                        OutputStream os = connection.getOutputStream();
-                        OutputStreamWriter osr = new OutputStreamWriter(os);
-                        BufferedWriter writer = new BufferedWriter(osr);
-                        writer.write(postText, 0, postText.length());
-                        writer.close();
-                    }
+                        connection.setRequestProperty("accept-charset", "UTF-8");
+                        if (postText != null) {
+                            connection
+                                .setRequestProperty("content-type", "application/x-www-form-urlencoded; charset=utf-8");
+                            connection.setRequestProperty("content-encoding", "UTF-8");
+                        }
 
-                    InputStream is = connection.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader reader = new BufferedReader(isr);
-                    StringBuilder result = new StringBuilder();
-
-                    while (true) {
-                        synchronized (HTTPRequest.this.m_lock) {
-                            if (HTTPRequest.this.m_cancelled) {
-                                break;
+                        if (headers != null) {
+                            for (Map.Entry<String, String> header : headers.entrySet()) {
+                                connection.setRequestProperty(header.getKey(), header.getValue());
                             }
                         }
 
-                        String line = reader.readLine();
-                        if (line == null) {
-                            break;
+                        if (postText != null) {
+                            connection.setDoOutput(true);
+                            OutputStream os = connection.getOutputStream();
+                            OutputStreamWriter osr = new OutputStreamWriter(os);
+                            BufferedWriter writer = new BufferedWriter(osr);
+                            writer.write(postText, 0, postText.length());
+                            writer.close();
                         }
-
-                        result.append(line);
-                        result.append('\n');
                     }
 
-                    reader.close();
-                    synchronized (HTTPRequest.this.m_lock) {
-                        if (HTTPRequest.this.m_cancelled) {
-                            HTTPRequest.this.m_complete = true;
-                            HTTPRequest.this.m_success = false;
-                            HTTPRequest.this.m_result = null;
+                    int code = responseCode = connection.getResponseCode();
+
+                    // If we get an error code then use the error stream instead
+                    InputStream is;
+                    boolean responseSuccess;
+                    if (code >= 200 && code < 400) {
+                        is = connection.getInputStream();
+                        responseSuccess = true;
+                    } else {
+                        is = connection.getErrorStream();
+                        responseSuccess = false;
+                    }
+
+                    // Read from the input stream
+                    ByteArrayOutputStream buffer = new ByteArrayOutputStream(Math.max(1024, is.available()));
+                    int nRead;
+                    byte[] data = new byte[1024];
+                    while ((nRead = is.read(data, 0, data.length)) != -1) {
+                        synchronized (lock) {
+                            if (cancelled) break;
+                        }
+
+                        buffer.write(data, 0, nRead);
+                    }
+                    is.close();
+
+                    synchronized (lock) {
+                        if (cancelled) {
+                            complete = true;
+                            success = false;
+                            result = null;
                         } else {
-                            HTTPRequest.this.m_complete = true;
-                            HTTPRequest.this.m_success = true;
-                            HTTPRequest.this.m_result = result.toString();
-                            HTTPRequest.this.m_responseCode = connection.getResponseCode();
+                            complete = true;
+                            success = responseSuccess;
+                            result = buffer.toByteArray();
+
+                            Map<String, Map<Integer, String>> headers = responseHeaders = new HashMap<String, Map<Integer, String>>();
+                            for (Map.Entry<String, List<String>> header : connection.getHeaderFields()
+                                .entrySet()) {
+                                Map<Integer, String> values = new HashMap<Integer, String>();
+
+                                int i = 0;
+                                for (String value : header.getValue()) {
+                                    values.put(++i, value);
+                                }
+
+                                headers.put(header.getKey(), values);
+                            }
                         }
                     }
 
                     connection.disconnect();
-                } catch (IOException var13) {
-                    synchronized (HTTPRequest.this.m_lock) {
-                        HTTPRequest.this.m_complete = true;
-                        HTTPRequest.this.m_success = false;
-                        HTTPRequest.this.m_result = null;
+                } catch (IOException e) {
+                    synchronized (lock) {
+                        complete = true;
+                        success = false;
+                        result = null;
+                    }
+                } catch (Exception e) {
+                    // Logger.error("Unknown exception fetching " + url, e);
+                    synchronized (lock) {
+                        complete = true;
+                        success = false;
+                        result = null;
                     }
                 }
             }
@@ -149,39 +181,31 @@ public class HTTPRequest {
     }
 
     public String getURL() {
-        return this.m_urlString;
+        return urlString;
     }
 
     public void cancel() {
-        synchronized (this.m_lock) {
-            this.m_cancelled = true;
+        synchronized (lock) {
+            cancelled = true;
         }
     }
 
     public boolean isComplete() {
-        synchronized (this.m_lock) {
-            return this.m_complete;
-        }
-    }
-
-    public int getResponseCode() {
-        synchronized (this.m_lock) {
-            return this.m_responseCode;
+        synchronized (lock) {
+            return complete;
         }
     }
 
     public boolean wasSuccessful() {
-        synchronized (this.m_lock) {
-            return this.m_success;
+        synchronized (lock) {
+            return success;
         }
     }
 
-    public BufferedReader getContents() {
-        String result = null;
-        synchronized (this.m_lock) {
-            result = this.m_result;
+    public HTTPResponse asResponse() {
+        synchronized (lock) {
+            return result == null ? null : new HTTPResponse(responseCode, result, responseHeaders);
         }
-
-        return result != null ? new BufferedReader(new StringReader(result)) : null;
     }
+
 }
