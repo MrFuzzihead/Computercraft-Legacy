945c9e13648e50c7830fcaf483eec0540a0c60a6
Duncan Campbell <campbellduncan12@gmail.com>
2026-09-16 20:26:49 -0500

    Q2 complete

--- src/main/java/dan200/computercraft/core/apis/PeripheralAPI.java
+++ src/main/java/dan200/computercraft/core/apis/PeripheralAPI.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import dan200.computercraft.ComputerCraft;
 import dan200.computercraft.api.filesystem.IMount;
 import dan200.computercraft.api.filesystem.IWritableMount;
 import dan200.computercraft.api.lua.ArgumentDelegator;
@@ -282,7 +283,8 @@ public class PeripheralAPI implements ILuaAPI, IAPIEnvironment.IPeripheralChange
             synchronized (this.m_fileSystem) {
                 return !this.m_fileSystem.exists(desiredLoc) ? desiredLoc : null;
             }
-        } catch (FileSystemException var5) {
+        } catch (FileSystemException e) {
+            ComputerCraft.logger.warn("ComputerCraft: failed to check peripheral mount location " + desiredLoc, e);
             return null;
         }
     }
@@ -401,7 +403,11 @@ public class PeripheralAPI implements ILuaAPI, IAPIEnvironment.IPeripheralChange
                     if (location != null) {
                         try {
                             PeripheralAPI.this.m_fileSystem.mount(driveName, location, mount);
-                        } catch (FileSystemException var8) {}
+                        } catch (FileSystemException e) {
+                            ComputerCraft.logger.warn(
+                                "ComputerCraft: failed to mount peripheral " + this.m_side + " at " + location, e);
+                            return null;
+                        }
                     }
                 }
 
@@ -429,7 +435,11 @@ public class PeripheralAPI implements ILuaAPI, IAPIEnvironment.IPeripheralChange
                     if (location != null) {
                         try {
                             PeripheralAPI.this.m_fileSystem.mountWritable(driveName, location, mount);
-                        } catch (FileSystemException var8) {}
+                        } catch (FileSystemException e) {
+                            ComputerCraft.logger.warn(
+                                "ComputerCraft: failed to mount writable peripheral " + this.m_side + " at " + location, e);
+                            return null;
+                        }
                     }
                 }
 
